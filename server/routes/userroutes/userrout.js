a46a8aede69ed679dd1730217b409ab08591c590
Memagog <55204188+Memagog@users.noreply.github.com>
2021-04-15 20:48:46 +0300

    validation

--- server/routes/userroutes/userrout.js
+++ server/routes/userroutes/userrout.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const userController = require('../../controller/user/userController');
-
+const {check} = require('express-validator');
 const router = express.Router();
 
-router.post('/registration',userController.registration)
+router.post('/registration',[
+       check("email"," email' should not be empty").notEmpty(),
+       check("password","It's not valid password").isLength({min:4 , max: 12}),
+],userController.registration)
 router.post('/login',userController.login)
 router.get('/auth',userController.auth)
 router.get('/users',userController.getAllUsers)
