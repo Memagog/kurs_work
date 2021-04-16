760abae5f49aa2eabd8e9970ce61a818c3689022
Memagog <55204188+Memagog@users.noreply.github.com>
2021-04-16 20:55:48 +0300

    create register and auth components

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -1,14 +1,51 @@
-import React from 'react';
+import React,{useState} from 'react';
 
-const Home = () => {
-    return(
-    <div>
-        <p>Hello Password</p>
-        <p>Hello Password</p>
-        <p>Hello Password</p>
-        <p>Hello Password</p>
-    </div>
-    )
+
+const Home = (props) => {
+    const [state, setState] = useState({
+        title: "",
+        message:"",
+        author:"",
+        tags: [],
+        selectField: "",
+        likeCounter: 0,
+        
+    });
+    const handleChange = event => {
+        setState({
+            ...state,
+            [event.target.name]: event.target.value
+        })
+    }
+    const handleSubmit = event => {
+        event.preventDefault();
+        
+        // axios.post('')
+
+    }
+    
+        return(
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        Title: 
+                        <input type="text" name="title" value={state.title} onChange={handleChange}/>   
+                    </label>
+                    <label>
+                        <p>Author: theme</p>
+                        <input type="text" name="author" value={state.author} onChange={handleChange}/>
+                    </label>
+                    <labe>
+                        
+                    </labe>
+                    <button type="submit">Add</button>
+                </form>
+                 <h1>{state.title}</h1> 
+                 <h1>{state.author}</h1> 
+            </div>       
+        );
+    
+   
+
 }
-
 export default Home;
