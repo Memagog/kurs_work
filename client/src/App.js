ba12b379e903746d02e8de85c437b4675d3bb643
Memagog <55204188+Memagog@users.noreply.github.com>
2021-04-17 13:42:39 +0300

    Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,58 +3,33 @@ import Home from './Components/Home/Home';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link,
+  Route,  
   Redirect
 } from "react-router-dom";
 import Login from './Components/Login/Login';
 import CreateEvent from './Components/CreateIventPage/CreateIvent';
 import Register from './Components/Registration/Register';
+import Navbar from './Components/Navbar/Navbar';
 function App() {
   return (
     <div>
-       <Router>
-      <div>        
-        <ul class="nav">
-        <li class="nav-item">
-        <Link class="nav-link active" to="/">Home</Link>
-        </li>
-        <li class="nav-item">
-        <Link class="nav-link" to="/login">Profile</Link>
-        </li>
-        <li class="nav-item">
-        <Link  class="nav-link" to="/create">CreateEvent</Link>
-        </li>
-        <li class="nav-item">
-        <Link class="nav-link disabled" href="/create">Disabled</Link>
-        </li>
-
-          {/* <nav class="navbar navbar-light bg-light">
-          <div class="container-fluid">          
-            <form class="d-flex">
-              <input class="form-control mr-2" type="search" placeholder="Search" aria-label="Search"/>
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-          </nav> */}
-      </ul>
-        <hr />
-        <Switch>
-          <Route exact path="/">
-           <Home/>
-          </Route>
-          <Route exact path="/login">
-            <Login/>
-          </Route>
-          <Route exact path="/create">
-            <CreateEvent/>
-          </Route>
-          <Route exact path="/register">
-           <Register/>
-          </Route>
-          <Redirect to="/"/>             
-        </Switch>
-      </div>
+       <Router>             
+        <Navbar/>
+          <Switch>
+            <Route exact path="/">
+            <Home/>
+            </Route>
+            <Route exact path="/login">
+              <Login/>
+            </Route>
+            <Route exact path="/create">
+              <CreateEvent/>
+            </Route>
+            <Route exact path="/register">
+            <Register/>
+            </Route>
+            <Redirect to="/"/>             
+          </Switch>      
     </Router>
     </div>
   );
