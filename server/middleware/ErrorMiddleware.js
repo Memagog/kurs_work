7f6859d9b3c314b8065d9885ec31051a190a9275
Memagog <55204188+Memagog@users.noreply.github.com>
2021-04-26 13:26:24 +0300

    Delete server directory

--- server/middleware/ErrorMiddleware.js
+++
@@ -1,8 +0,0 @@
-const ApiError = require("../apiError/errors")
-
-module.exports = function(req,res,next){
-    if(err instanceof ApiError){
-      return res.status(err.status).json(err.message)
-    }
-    return res.status(500).json({message: `Error has been unique`})
-}
--- server/routes/router.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userRouter = require('./userroutes/userrout');
-const eventRouter = require('./eventroutes/eventrout');
-router.use('/user',userRouter);
-router.use('/events', eventRouter);
-module.exports =  router;
